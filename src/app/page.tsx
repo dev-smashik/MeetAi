fa180b3b6a3f04227c4e2c0f349afb2d9781d9ef
dev-smashik <dev.smashik.com>
2025-05-29 18:14:31 +0600

    Initialize Database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-4xl font-bold text-green-500">
-      Hello Ashik
-    </h1>
+    <Button className="bg-blue-500 text-white p-4 rounded hover:bg-black">
+      Click Me!
+    </Button>
   );
-}
+
+};
