35d9f37fbb953a475bda10dc9a3fd93b64fad095
dev-smashik <dev.smashik@gmail.com>
2025-06-06 16:54:36 +0600

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { HomeView } from "@/modules/home/ui/views/home-view";
-import { auth } from "@/lib/auth-client";
+// import { auth } from "@/lib/auth-client";
 import { headers } from "next/headers";
 
 const Page = () => {
-  const session = await auth.api.getSession(({
+  // const session = await auth.api.getSession(({
 
-  }))
+  // }))
   return <HomeView />;
 }
 
